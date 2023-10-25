675a0419ef26b6aacfc4c5e623a9f6a0176abb81
MaarikaK <testmaarika@gmail.com>
2023-10-25 23:10:06 +0300

    add my first app

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿//This is comment
+
+string myName = "Frank Jeppe";
+string myLastName = "Kaus";
+
+//Console.WriteLine("Hello" + "," + " " + myName);
+Console.WriteLine($"Hello,{myName} {myLastName}");
